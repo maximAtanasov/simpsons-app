cc02700fd65ff52b6636597e2f247abf0e306c57
Maxim Atanasov <maxim.atanasov@protonmail.com>
2025-02-26 22:59:16 +0100

    Implemented additional functionality to fetch and store quotes from the simpson quote API.

--- simpson-server/app/Http/Controllers/QuoteController.php
+++ simpson-server/app/Http/Controllers/QuoteController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\CharacterDirection;
 use App\Models\Quote;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
 
 class QuoteController extends Controller
 {
@@ -14,7 +16,39 @@ class QuoteController extends Controller
     {
         //TODO: Move to a service layer?
         //TODO: Test
+        $this->fetchAndStoreQuote();
         $quotes = Quote::query()->orderBy('created_at', 'desc')->limit(5)->get();
         return response()->json($quotes);
+    }
+
+    /**
+     * Fetches and stores quotes from the thesimpsonsquoteapi. If there are more than 5 quotes
+     * persisted, then the oldest one is deleted.
+     * @return void
+     */
+    private function fetchAndStoreQuote(): void
+    {
+        $response = Http::get('https://thesimpsonsquoteapi.glitch.me/quotes');
+
+        if ($response->successful()) {
+            $simpsonQuotes = $response->json();
+
+            if (isset($simpsonQuotes[0])) {
+                $quoteData = $simpsonQuotes[0];
+
+                if (Quote::count() >= 5) {
+                    Quote::oldest()->first()?->delete();
+                }
+
+                Quote::create([
+                    'text' => $quoteData['quote'],
+                    'character' => $quoteData['character'],
+                    'image_url' => $quoteData['image'],
+                    'character_direction' => CharacterDirection::from($quoteData['characterDirection'])
+                ]);
+            }
+        } else {
+            //TODO: Log error
+        }
     }
 }
